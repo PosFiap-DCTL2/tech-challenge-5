dfe4f937181a9fa9372df3c04a457e5d922a4a65
DaniloRferraz <danilor.ferraz@outlook.com>
2026-08-19 22:16:32 +0000

    .

--- Terraform/Networking/main.tf
+++ Terraform/Networking/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "hackathonvpc" {
 #### SUBNETS ####
 
 resource "aws_subnet" "subnetpublica1" {
-  vpc_id                  = aws_default_vpc.hackathonvpc.id
+  vpc_id                  = aws_vpc.hackathonvpc.id
   cidr_block              = "172.16.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = var.availability_zones[0]
@@ -22,7 +22,7 @@ resource "aws_subnet" "subnetpublica1" {
 }
 
 resource "aws_subnet" "subnetpublica2" {
-  vpc_id                  = aws_default_vpc.hackathonvpc.id
+  vpc_id                  = aws_vpc.hackathonvpc.id
   cidr_block              = "172.16.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = var.availability_zones[1]
@@ -35,7 +35,7 @@ resource "aws_subnet" "subnetpublica2" {
 }
 
 resource "aws_subnet" "subnetprivada1" {
-  vpc_id                  = aws_default_vpc.hackathonvpc.id
+  vpc_id                  = aws_vpc.hackathonvpc.id
   cidr_block              = "172.16.3.0/24"
   map_public_ip_on_launch = false
   availability_zone       = var.availability_zones[0]
@@ -48,7 +48,7 @@ resource "aws_subnet" "subnetprivada1" {
 }
 
 resource "aws_subnet" "subnetprivada2" {
-  vpc_id                  = aws_default_vpc.hackathonvpc.id
+  vpc_id                  = aws_vpc.hackathonvpc.id
   cidr_block              = "172.16.4.0/24"
   map_public_ip_on_launch = false
   availability_zone       = var.availability_zones[1]
