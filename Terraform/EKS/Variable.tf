c6ca5aadad1d29e37202a451d5b9adc73f1086f0
DaniloRferraz <danilor.ferraz@outlook.com>
2026-08-19 22:28:37 +0000

    Alterando as labroles

--- Terraform/EKS/Variable.tf
+++ Terraform/EKS/Variable.tf
@@ -7,7 +7,7 @@ variable "instance_types" {
 variable "lab_role_arn" {
   description = "ARN of the AWS Academy LabRoles"
   type        = string
-  default     = "arn:aws:iam::082722225763:role/admin"
+  default     = "arn:aws:iam::401260453914:role/LabRole
 }
 
 variable "subnets" {
